5b317f689778a6de89e3120217136898b77c90bf
Your Name <you@example.com>
2016-10-02 22:52:51 +0300

    v1.1

--- task1/Main.java
+++ task1/Main.java
@@ -1,10 +1,9 @@
 package com.company;
-
+//task1
 public class Main {
-
-    public static void main(String[] args) {  //enter the argument from the command line
-	for(int i = args.length-1; i>=0; i--){
-        System.out.println("Аргумент: "+ i + args[i]);
+  public static void main(String[] args) { //enter the argument from the command line
+    for (int i = args.length - 1; i >= 0; i--) {//in reverse order
+      System.out.println("Argument: " + i + args[i]); //result output
     }
-    }
+  }
 }
