2de5699487ac199c5bb9521fc1edda0518bb8b56
Your Name <you@example.com>
2016-10-02 19:03:49 +0300

    add task2

---
+++ task2/MyName.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Admin on 02.10.2016.
+ */
+public class MyName {
+    public static void main(String args[]){
+        if(args.length!=0) System.out.println("Hello, "+args+".");
+        else System.out.println("It isn't name.");
+  }
+
+}
