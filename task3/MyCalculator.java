c1c14a258a507538c6c4f8979440365a12cdfcfc
Your Name <you@example.com>
2016-10-03 16:03:23 +0300

    v1.2

--- task3/MyCalculator.java
+++ task3/MyCalculator.java
@@ -1,22 +1,21 @@
 package com.company;
+/*class MyCalculator does all calculations.*/
 public class MyCalculator {
-  private double check(double num1, double num2) { //method check checks 0/0 and number/0
+  public int check(double num1, double num2) { //method check checks 0/0 and number/0
     if (Double.isNaN(num1 / num2)|| Double.isInfinite(num1 / num2))
       return 0;
     else return 1;
   }
-  double add(double num1, double num2) { //method add counts the sum
+  public double add(double num1, double num2) { //method add counts the sum
     return num1 + num2;
   }
-  double subtract(double num1, double num2) { //method subtract counts the rest
+  public double subtract(double num1, double num2) { //method subtract counts the rest
     return num1 - num2;
   }
-  double multiply(double num1, double num2) { ///method multiply counts the product
+  public double multiply(double num1, double num2) { ///method multiply counts the product
     return num1 * num2;
   }
-  double divide(double num1, double num2){ //method divide counts the quotients
-    if (check(num1, num2) == 1)
+  public double divide(double num1, double num2){ //method divide counts the quotients
       return num1 / num2;
-    else return 0;
   }
 }
