059a625b9837cc8f3b0b9733b37b68098578c3a1
Your Name <you@example.com>
2016-10-03 16:49:17 +0300

    t2v1.3

--- task2/MyName.java
+++ task2/MyName.java
@@ -1,5 +1,8 @@
 public class MyName {
-  public static void main(String args[]) {
+  /*
+   *print Hello, args, args enter from command line
+   */
+  public static void main(String args[]) { //output Hello, args
     if (args.length != 0) { //name enter
       for (int i = 0; i < args.length; i++) {
         System.out.println("Hello, " + args[i] + "."); //output
