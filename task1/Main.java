3dbb68cf15351e578ed819cc4c721b41c9371928
Your Name <you@example.com>
2016-10-03 16:05:10 +0300

    v1.2

--- task1/Main.java
+++ task1/Main.java
@@ -1,8 +1,8 @@
 package com.company;
-//task1
+//class Main out puts in revers order
 public class Main {
   public static void main(String[] args) { //enter the argument from the command line
-    for (int i = args.length - 1; i >= 0; i--) {//in reverse order
+    for (int i = args.length - 1; i >= 0; i--) {
       System.out.println("Argument: " + i + args[i]); //result output
     }
   }
