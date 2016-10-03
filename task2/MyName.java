49032c7f6a79b5d56b40a419e5d08ae95dfcc719
Your Name <you@example.com>
2016-10-03 16:06:52 +0300

    v1.2

--- task2/MyName.java
+++ task2/MyName.java
@@ -1,9 +1,12 @@
 public class MyName {
   public static void main(String args[]) {
     if (args.length != 0) { //name enter
-      for (int i = 0; i < args.length; i++)
-      System.out.println("Hello, " + args[i] + "."); //output
+      for (int i = 0; i < args.length; i++) {
+        System.out.println("Hello, " + args[i] + "."); //output
+      }
     }
-    else System.out.println("It isn't name."); //name isn't
+    else {
+      System.out.println("It isn't name."); //name isn't
+    }
   }
 }
